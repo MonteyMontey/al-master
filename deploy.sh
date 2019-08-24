e1eb353de94f8df2bec004fa1b70d81025d5afd6
monteymontey <dominik.klein@code.berlin>
2019-08-24 14:38:20 +0200

    remove old docker images fix

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-# a script that:
-# - (0) zips and uploads the new image to compute engine instance
-# - (1) ssh into the instance
-# - (2) replaces the old docker image 
-# - (3) removes uploaded .tar.gz. file
-# - (4) and restarts the master service
 
 sudo docker save al-master | gzip > al-master.tar.gz &&
 scp -i ./deploy_key -o StrictHostKeyChecking=no al-master.tar.gz montey@35.246.168.135:/tmp &&
 ssh -i ./deploy_key -o StrictHostKeyChecking=no montey@35.246.168.135 << EOF
-sudo docker rmi -f $(docker images -a -q);
+sudo systemctl stop master.service;
 cat /tmp/al-master.tar.gz | gunzip | sudo docker load;
 rm /tmp/al-master.tar.gz;
 sudo systemctl enable master;
 sudo systemctl restart master;
+sudo docker system prune -f
 EOF
